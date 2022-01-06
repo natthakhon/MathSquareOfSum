39cdda43c83a4f05821a6c31646a1a8d9b4e7a71
gone <nattlao@gmail.com>
2022-01-06 11:00:19 +0700

    commit

---
+++ MathSquareOfSumLib/DesignerMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Activities.Presentation.Metadata;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSquareOfSumLib
+{
+    public class DesignerMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+            AttributeTableBuilder attributeTableBuilder = new AttributeTableBuilder();
+            attributeTableBuilder.AddCustomAttributes(typeof(MathSqSum), new DesignerAttribute(typeof(MathSqSumDesigner)));
+            MetadataStore.AddAttributeTable(attributeTableBuilder.CreateTable());
+        }
+    }
+}
---
+++ MathSquareOfSumLib/MathSqSum .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSquareOfSumLib
+{   
+    public class MathSqSum : CodeActivity
+    {
+        [Category("Input")]
+        [DisplayName("First Number")]
+        [Description("Enter the first number")]
+        [RequiredArgument]
+        public InArgument<int> FirstNumber { get; set; }
+
+        [Category("Input")]
+        [DisplayName("Second Number")]
+        [Description("Enter the second number")]
+        [RequiredArgument]
+        public InArgument<int> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> ResultNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+
+            var result = System.Math.Pow(firstNumber + secondNumber, 2);
+            ResultNumber.Set(context, result);
+        }
+    }
+}
